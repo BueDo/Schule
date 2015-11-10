486041a1f42dbf2da82972c1380fad6eb1ec8406
BueDo <doom4711@gmail.com>
2015-11-10 20:17:28 +0100

    Potenzieren erstellt; Algorythmus muss erst implemetiert werden

---
+++ src/taschenrechner/Potenzieren.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taschenrechner;
+
+/**
+ *
+ * @author Domi
+ */
+public class Potenzieren implements Berechnung {
+    
+    @Override
+    public double berechnen(double a, double b){
+        //TODO berechnung implementieren
+        return 42.0;
+    }
+}
+    
+
